591afcb4280b3fb6ba43de4ab0453c2944c718d2
Robinson_Xavier <robinson.webzen@gmail.com>
2022-09-10 12:09:51 -0300

    Start

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../styles/reset.css';
+import '../styles/global.css';
+
+import Login from './Login';
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Login () {
+    return (
+        <>
+            <LoginPage>
+                <div>
+                    <h1>MyWallet</h1>
+                </div>
+                <form>
+                    <input />
+                    <input />
+                    <button></button>
+                </form>
+            </LoginPage>
+        </>
+    )
+};
+
+const LoginPage = styled.div`
+    
+`;
